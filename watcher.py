ed13c82cebc70922e7cc57dde573d7f1d649b0b0
CharliePoluden <poluden33@gmail.com>
2026-05-31 11:38:08 +0300

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -1,12 +1,10 @@
 import os
-import time
+import re
 import requests
 
 USERNAME = os.getenv("USERNAME")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-
-CHECK_INTERVAL = 30
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0"
@@ -22,40 +20,17 @@ def notify(message):
         timeout=10
     )
 
-def check_account():
-    url = f"https://x.com/{USERNAME}"
+url = f"https://x.com/{USERNAME}"
 
-    response = requests.get(url, headers=HEADERS, timeout=15)
-    text = response.text.lower()
+response = requests.get(url, headers=HEADERS, timeout=15)
+html = response.text
 
-    if "account suspended" in text:
-        return False
+title_match = re.search(r"<title>(.*?)</title>", html, re.IGNORECASE | re.DOTALL)
 
-    return True
+title = title_match.group(1).strip() if title_match else "NO TITLE"
 
-
-current_state = check_account()
-
-if current_state:
-    notify(f"Watcher started\n@{USERNAME} СЕЙЧАС ДОСТУПЕН")
-else:
-    notify(f"Watcher started\n@{USERNAME} СЕЙЧАС ЗАБЛОКИРОВАН")
-
-last_state = current_state
-
-while True:
-    try:
-        current_state = check_account()
-
-        if current_state != last_state:
-            if current_state:
-                notify(f"🚨 @{USERNAME} РАЗБЛОКИРОВАН")
-            else:
-                notify(f"⚠️ @{USERNAME} СНОВА ЗАБЛОКИРОВАН")
-
-            last_state = current_state
-
-    except Exception as e:
-        notify(f"Ошибка: {e}")
-
-    time.sleep(CHECK_INTERVAL)
+notify(
+    f"STATUS: {response.status_code}\n"
+    f"TITLE: {title}\n"
+    f"HTML SAMPLE:\n{html[:500]}"
+)
